00ef7c8631faa729158867035634a9baa940a2c1
Louis Peter <louisclarencepeters@gmail.com>
2023-08-29 14:34:34 +0200

    added a cookie banner

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cookieBanner = document.getElementById("cookieBanner");
+    const acceptButton = document.getElementById("acceptButton");
+
+    acceptButton.addEventListener("click", function () {
+        cookieBanner.style.display = "none";
+    });
+});
